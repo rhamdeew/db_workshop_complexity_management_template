30b4f7ff59d1e81d733a4b54f0669aa9141b4b40
Rail Hamdeev <rail@hamdeew.ru>
2021-01-30 17:27:31 +0400

    add state to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  include AASM
+
   has_many :comments, dependent: :destroy
   belongs_to :category
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  aasm column: :state do
+    state :draft, initial: true
+    state :published
+    state :archived
+
+    event :publish do
+      transitions from: :draft, to: :published
+    end
+
+    event :archive do
+      transitions from: :published, to: :archived
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_101635) do
+ActiveRecord::Schema.define(version: 2021_01_30_132119) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
